27f0c1cd9e026415a7865d6274f0b75cd7bf6820
downpour07 <choiheeyoung10@gmail.com>
2024-09-03 10:29:29 +0900

    ⚡️ :: joystick

--- main/main.ino
+++ main/main.ino
@@ -9,6 +9,42 @@ void setup() {
 }
 
 void loop() {
+  //joystick
+  int currX = analogRead(jsX);
+  int currY = analogRead(jsY);
+
+  //left
+  if(currX == 1023) {
+    Keyboard.press(KEY_LEFT_ARROW);
+  }
+  else {
+    Keyboard.release(KEY_LEFT_ARROW);
+  }
+
+  //right
+  if(currX == 0) {
+    Keyboard.press(KEY_RIGHT_ARROW);
+  }
+  else {
+    Keyboard.release(KEY_RIGHT_ARROW);
+  }
+
+  //up
+  if(currY == 1023) {
+    Keyboard.press(KEY_UP_ARROW);
+  }
+  else {
+    Keyboard.release(KEY_UP_ARROW);
+  }
+
+  //down
+  if(currY == 0) {
+    Keyboard.press(KEY_DOWN_ARROW);
+  }
+  else {
+    Keyboard.release(KEY_DOWN_ARROW);
+  }
+
   //button
   int buttonControl = digitalRead(button);
 
